710244f578831527e2cf1eb170b75086b200986f
Noble Obioma <nobioma1@gmail.com>
2019-06-03 14:55:49 +0100

    Import Header, PostContainer and SideBar and Style main container

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/HeaderContainer/Header';
+import PostContainer from './Components/PostContainer/PostContainer';
+import SideBar from './Components/SideBar/SideBar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
+        <main className="main-container">
+          <PostContainer />
+          <SideBar />
+        </main>
       </div>
     );
   }
