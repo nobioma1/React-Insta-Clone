3ba22268f076897cae2b25cc2d296f0d2e12c119
Noble Obioma <nobioma1@gmail.com>
2019-06-03 14:50:56 +0100

    Create Search Component and style component

---
+++ instagram-app/src/Components/SearchBar/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './search.css';
+
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search" />
+    </div>
+  );
+};
+
+export default Search;
