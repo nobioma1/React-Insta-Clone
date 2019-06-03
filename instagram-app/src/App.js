b2539ba246b96306cc6b10ce21fb65540f117a2d
Noble Obioma <nobioma1@gmail.com>
2019-06-03 15:41:16 +0100

    import dummyData and set it to state, pass state data as prop into PostContainer Component

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Header from './Components/HeaderContainer/Header';
 import PostContainer from './Components/PostContainer/PostContainer';
 import SideBar from './Components/SideBar/SideBar';
+import dummyData from './dummy-data';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      appData: dummyData
+    }
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <main className="main-container">
-          <PostContainer />
+          <PostContainer appData={this.state.appData} />
           <SideBar />
         </main>
       </div>
