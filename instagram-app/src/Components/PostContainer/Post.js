cae55aceaec62c754a27619421d181b2b74d0b03
Noble Obioma <nobioma1@gmail.com>
2019-06-03 15:45:58 +0100

    desturcture user info passed in and replace placeholders

--- instagram-app/src/Components/PostContainer/Post.js
+++ instagram-app/src/Components/PostContainer/Post.js
@@ -4,17 +4,18 @@ import Comments from '../CommentSection/Comments';
 import { FiHeart, FiShare } from 'react-icons/fi';
 import { FaRegComment } from 'react-icons/fa';
 
-const Post = () => {
+const Post = props => {
+  const { username, thumbnailUrl, imageUrl, likes, timestamp, comments } = props.userInfo;
   return (
     <div className="post">
       <div className="post-header">
         <div className="avatar-container">
-          <img src="" alt="" />
+          <img src={thumbnailUrl} alt="avatar" />
         </div>
-        <h4>nobleobioma_</h4>
+        <h4>{username}</h4>
       </div>
       <div className="image-container">
-        <img src="image.jpg" alt="" />
+        <img src={imageUrl} alt="post" />
         <div className="image-buttons" />
         <div className="post-details">
           <div className="post-icons">
@@ -28,17 +29,15 @@ const Post = () => {
               <FiShare />
             </i>
           </div>
-          <p className="like-count">100 likes</p>
+          <p className="like-count">{`${likes} likes`}</p>
           <p className="details">
-            <span>nobleobioma</span>Thank you for being my best friend. Thank
-            you for your understanding. Thank you for stomaching my nonsense.
-            Thank you...
+            <span>{username}</span>
           </p>
         </div>
       </div>
       <div className="post-footer">
-        <Comments />
-        <p className="time">2 HOURS AGO</p>
+        <Comments postComments={comments}/>
+        <p className="time">{timestamp}</p>
       </div>
       <AddComment />
     </div>
