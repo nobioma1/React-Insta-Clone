832c085512e81e1989116776df9db9c6ae1defa2
Noble Obioma <nobioma1@gmail.com>
2019-06-06 14:09:01 +0100

    Refactor Comment and Comments Components styling to styled-components and delete comments css

--- instagram-app/src/Components/CommentSection/Comments.js
+++ instagram-app/src/Components/CommentSection/Comments.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Comment from './Comment';
-import './comments.css';
+
+const CommentsSection = styled.div`
+  padding-bottom: 5px;
+
+  div {
+    width: 100%;
+    font-size: 2.5rem;
+
+    span {
+      font-weight: bold;
+      font-size: 1.5rem;
+      padding-right: 5px;
+    }
+  }
+`;
 
 const Comments = props => {
   return (
-    <div className="comment-section">
+    <CommentsSection>
       {props.postComments.map(comment => (
         <div key={comment.id}>
           <Comment comment={comment} />
         </div>
       ))}
-    </div>
+    </CommentsSection>
   );
 };
 
 Comments.propTypes = {
-  postComments: PropTypes.arrayOf(PropTypes.shape({
-    username: PropTypes.string,
-    text: PropTypes.string,
-  })).isRequired
-}
+  postComments: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      text: PropTypes.string
+    })
+  ).isRequired
+};
 
 Comments.defaultProps = {
   postComments: []
-}
+};
 
 export default Comments;
